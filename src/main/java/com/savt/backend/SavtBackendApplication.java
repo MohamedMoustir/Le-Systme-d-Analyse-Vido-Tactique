77d41aff41971a8d5f054f9cb14842265cbaa4e0
MohamedMoustir <itsmoustir@gmail.com>
2025-12-16 16:56:23 +0100

    inti project

---
+++ src/main/java/com/savt/backend/SavtBackendApplication.java
@@ -0,0 +1,13 @@
+package com.savt.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SavtBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SavtBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/savt/backend/SavtBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.savt.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SavtBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

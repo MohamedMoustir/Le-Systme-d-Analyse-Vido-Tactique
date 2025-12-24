4235c3318161f2a69e74d9d2285429e6cc360231
MohamedMoustir <itsmoustir@gmail.com>
2025-12-24 14:57:37 +0100

    feat(domain): implement Equipe and Joueur entities

--- src/main/java/com/savt/backend/domain/entity/Equipe.java
+++ src/main/java/com/savt/backend/domain/entity/Equipe.java
@@ -1,4 +1,19 @@
 package com.savt.backend.domain.entity;
 
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection="equipes")
+@Data
+@Builder
 public class Equipe {
+
+    @Id
+    private String id;
+
+    private String nom;
+    private String logoUrl;
+    private String couleurHex;
 }
--- src/main/java/com/savt/backend/domain/entity/Joueur.java
+++ src/main/java/com/savt/backend/domain/entity/Joueur.java
@@ -1,4 +1,18 @@
 package com.savt.backend.domain.entity;
 
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Document(collection = "joueurs")
 public class Joueur {
+    @Id
+    private String id;
+
+    private String nomComplet;
+    private Integer numeroMaillot;
+    private String poste;
+
+    private String equipeId;
 }

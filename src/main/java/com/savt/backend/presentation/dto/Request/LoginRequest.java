f090492ad2747c2affc57ee96ad6009248cb9f3c
MohamedMoustir <itsmoustir@gmail.com>
2025-12-24 14:57:55 +0100

    feat(dto): add login and register request DTOs

--- src/main/java/com/savt/backend/presentation/dto/Request/LoginRequest.java
+++ src/main/java/com/savt/backend/presentation/dto/Request/LoginRequest.java
@@ -1,4 +1,11 @@
 package com.savt.backend.presentation.dto.Request;
 
+import lombok.Data;
+
+@Data
 public class LoginRequest {
+    @NotBlank
+    private String email;
+    @NotBlank
+    private String password;
 }
--- src/main/java/com/savt/backend/presentation/dto/Request/RegisterRequest.java
+++ src/main/java/com/savt/backend/presentation/dto/Request/RegisterRequest.java
@@ -1,4 +1,18 @@
 package com.savt.backend.presentation.dto.Request;
 
+import lombok.Data;
+
+@Data
 public class RegisterRequest {
+    @NotBlank(message = "Le nom est obligatoire")
+    private String nom;
+
+    @Email(message = "Email invalide")
+    @NotBlank
+    private String email;
+
+    @NotBlank
+    private String password;
+
+    private String role;
 }

aaffe3f497fbeb09ea0a4a84d061284196ab9de7
MohamedMoustir <itsmoustir@gmail.com>
2025-12-24 14:58:11 +0100

    feat(dto): add position data request structure

--- src/main/java/com/savt/backend/presentation/dto/Request/PositionDataRequest.java
+++ src/main/java/com/savt/backend/presentation/dto/Request/PositionDataRequest.java
@@ -1,4 +1,13 @@
 package com.savt.backend.presentation.dto.Request;
 
+import lombok.Data;
+
+@Data
 public class PositionDataRequest {
+    private Long frameNumber;
+    private Float x;
+    private Float y;
+    private Float speed;
+    private String joueurId;
+
 }

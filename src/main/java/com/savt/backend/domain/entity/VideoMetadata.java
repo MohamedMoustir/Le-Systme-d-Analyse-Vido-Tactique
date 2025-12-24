3d5c6d9b7d7b5e81d58ce112ffadc376afefb59e
MohamedMoustir <itsmoustir@gmail.com>
2025-12-24 14:57:41 +0100

    feat(domain): implement VideoMetadata entity

--- src/main/java/com/savt/backend/domain/entity/VideoMetadata.java
+++ src/main/java/com/savt/backend/domain/entity/VideoMetadata.java
@@ -1,4 +1,30 @@
 package com.savt.backend.domain.entity;
 
+import com.savt.backend.domain.enums.StatutAnalyse;
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+
+@Data
+@Document(collection="analyses")
 public class VideoMetadata {
+
+    @Id
+    private String id;
+
+    private String titre;
+    private String urlFichier;
+    private Long dureeSecondes;
+    private LocalDateTime dateUpload;
+
+    private StatutAnalyse statut;
+
+    @Indexed
+    private String uploaderId;
+
+    private List<EvenementMatch> events = new ArrayList<>();
+    private List<Commentaire> commentaires = new ArrayList<>();
 }

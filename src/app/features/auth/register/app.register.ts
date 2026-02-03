d4d7cd1f743d99909b1a0b7d511d08f3d41014ae
MohamedMoustir <itsmoustir@gmail.com>
2026-02-03 16:26:09 +0100

    feat(auth): improve register UI and validation

--- src/app/features/auth/register/app.register.ts
+++ src/app/features/auth/register/app.register.ts
@@ -3,6 +3,7 @@ import { Component, inject } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
 import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Auth } from '../../../core/services/auth';
+import { AuthStore } from '../../../core/store/auth.store';
 @Component({
   selector: 'app-register',
   imports: [CommonModule, ReactiveFormsModule, RouterModule],
@@ -12,8 +13,7 @@ import { Auth } from '../../../core/services/auth';
 export class Register {
 
   private fb = inject(FormBuilder);
-  private authService = inject(Auth);
-  private router = inject(Router);
+  readonly store = inject(AuthStore);
 
   registerForm: FormGroup = this.fb.group({
     nom: ['', [Validators.required, Validators.minLength(3)]],
@@ -21,33 +21,20 @@ export class Register {
     password: ['', [Validators.required, Validators.minLength(6)]],
     role: ['COACH', [Validators.required]]
   });
-  isLoading = false;
-  errorMessage = '';
-  get f() {return this.registerForm.controls;}
+
+
+  get f() { return this.registerForm.controls; }
 
   onSubmit() {
-    if(this.registerForm.invalid){
-  this.registerForm.markAllAsTouched();
-  return;
+    if (this.registerForm.invalid) {
+      this.registerForm.markAllAsTouched();
+      return;
     }
-    this.isLoading = true;
-    this.errorMessage = '';
     const userData = this.registerForm.value;
-
-    this.authService.register(userData).subscribe({
-      next: ()=>{
-        this.isLoading = false;
-        this.router.navigate(['/login']);
-
-      },
-      error: (err)=>{
-        this.isLoading = false;
-        this.errorMessage = err?.error?.message || 'Erreur lors de l\'inscription. Veuillez réessayer.';
-      }
-    })
+    this.store.register(userData);
 
   }
   loginWithGoogle() {
-  console.log("Tentative de connexion avec Google...");
-}
+    console.log("Tentative de connexion avec Google...");
+  }
 }

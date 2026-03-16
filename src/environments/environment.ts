858ee0d30e994c9c1b31c0d9063be0eddd7fd55c
MohamedMoustir <itsmoustir@gmail.com>
2026-03-16 10:19:26 +0000

    fix: update video and stream URLs to server IP

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl: '/api', 
-  wsUrl: '/ws-analysis',
-  streamUrl: '/stream',
-  uploadsUrl: '/uploads'
+ apiUrl: 'http://165.232.38.245/api',
+  uploadsUrl: 'http://165.232.38.245/uploads',
+  streamUrl: 'http://165.232.38.245/api/analysis/stream',
+  wsUrl: 'ws://165.232.38.245/ws-analysis'
 };

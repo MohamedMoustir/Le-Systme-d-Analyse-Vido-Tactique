8f3964a6d06a73a815dbc36c42ac9b1e154b1c5b
MohamedMoustir <itsmoustir@gmail.com>
2026-03-17 12:08:50 +0000

    refactor(dto): update JoueurUpdateDTO schema

--- src/main/java/com/football/analyzer/presentation/dto/Response/JoueurUpdateDTO.java
+++ src/main/java/com/football/analyzer/presentation/dto/Response/JoueurUpdateDTO.java
@@ -1,4 +1,4 @@
-package com.football.analyzer.presentation.dto.response;
+package com.football.analyzer.presentation.dto.Response;
 
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;

ad1fc971f207a86509c56510615af498a1b967b1
MohamedMoustir <itsmoustir@gmail.com>
2026-03-17 12:08:48 +0000

    refactor(dto): update FrameAnalysisDTO schema

--- src/main/java/com/football/analyzer/presentation/dto/Response/FrameAnalysisDTO.java
+++ src/main/java/com/football/analyzer/presentation/dto/Response/FrameAnalysisDTO.java
@@ -1,4 +1,4 @@
-package com.football.analyzer.presentation.dto.response;
+package com.football.analyzer.presentation.dto.Response;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;

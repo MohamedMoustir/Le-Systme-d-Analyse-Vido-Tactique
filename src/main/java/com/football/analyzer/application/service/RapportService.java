be6d904a4f6bfc8c2424e7a0ec51147b6da18bd1
MohamedMoustir <itsmoustir@gmail.com>
2026-03-17 12:08:13 +0000

    refactor(service): update RapportService interface

--- src/main/java/com/football/analyzer/application/service/RapportService.java
+++ src/main/java/com/football/analyzer/application/service/RapportService.java
@@ -1,6 +1,6 @@
 package com.football.analyzer.application.service;
 
-import com.football.analyzer.presentation.dto.response.RapportGlobalResponse;
+import com.football.analyzer.presentation.dto.Response.RapportGlobalResponse;
 
 public interface RapportService {
     RapportGlobalResponse getMyStats(String userId);

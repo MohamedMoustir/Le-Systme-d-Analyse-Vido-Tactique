65b75d2f20dae223788886f8d1da418b64cf17b7
MohamedMoustir <itsmoustir@gmail.com>
2026-03-17 12:08:10 +0000

    refactor(service): update AuthService interface

--- src/main/java/com/football/analyzer/application/service/AuthService.java
+++ src/main/java/com/football/analyzer/application/service/AuthService.java
@@ -1,7 +1,7 @@
 package com.football.analyzer.application.service;
 
-import com.football.analyzer.presentation.dto.request.RegisterRequest;
-import com.football.analyzer.presentation.dto.response.LoginResponse;
+import com.football.analyzer.presentation.dto.Request.RegisterRequest;
+import com.football.analyzer.presentation.dto.Response.LoginResponse;
 
 public interface AuthService {
      LoginResponse register(RegisterRequest registerRequest);

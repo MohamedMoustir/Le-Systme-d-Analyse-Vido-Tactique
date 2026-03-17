bfabc2717e640bdc700c7afc1bc930b3ff4f75a1
MohamedMoustir <itsmoustir@gmail.com>
2026-03-17 12:08:51 +0000

    refactor(dto): update LoginResponse schema

--- src/main/java/com/football/analyzer/presentation/dto/Response/LoginResponse.java
+++ src/main/java/com/football/analyzer/presentation/dto/Response/LoginResponse.java
@@ -1,4 +1,4 @@
-package com.football.analyzer.presentation.dto.response;
+package com.football.analyzer.presentation.dto.Response;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;

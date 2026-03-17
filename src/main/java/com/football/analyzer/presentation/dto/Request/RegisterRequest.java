c9ff4837ef8a6297125d76bb857794d7004eac00
MohamedMoustir <itsmoustir@gmail.com>
2026-03-17 12:08:41 +0000

    refactor(dto): update RegisterRequest schema

--- src/main/java/com/football/analyzer/presentation/dto/Request/RegisterRequest.java
+++ src/main/java/com/football/analyzer/presentation/dto/Request/RegisterRequest.java
@@ -1,4 +1,4 @@
-package com.football.analyzer.presentation.dto.request;
+package com.football.analyzer.presentation.dto.Request;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;

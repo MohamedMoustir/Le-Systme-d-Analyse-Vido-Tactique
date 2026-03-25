b2761a28cd2286cd7c3689d1b372c10db7f37ec3
MohamedMoustir <itsmoustir@gmail.com>
2026-03-25 15:04:15 +0100

    feat(video-library): implement computed signal for real-time filtering

--- frontend/src/app/features/video-library/video-library.ts
+++ frontend/src/app/features/video-library/video-library.ts
@@ -1,26 +1,41 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, computed, inject, OnInit, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { VideoStore } from '../../core/store/video.store';
 import { SidebarComponent } from "../../core/layout/sidebar/app-sidebar";
 import { environment } from '../../../environments/environment';
+import { filter } from 'rxjs';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-video-library',
   standalone: true,
-  imports: [CommonModule, RouterLink, SidebarComponent],
+  imports: [CommonModule, RouterLink, SidebarComponent,FormsModule],
   templateUrl: './video-library.html'
 })
 export class VideoLibraryComponent implements OnInit {
   readonly store = inject(VideoStore);
   private cacheBuster = new Date().getTime();
+  searchTerm=signal("");
+
 
   ngOnInit() {
     this.store.loadUserVideos();
   }
 
+  filterVideo = computed(() => {
+    const trim = this.searchTerm().toLowerCase().trim();
+
+    if (!trim) this.store.userVideos();
+    return this.store.userVideos().filter(f => {
+      return f.titre.toLowerCase().includes(trim);
+    })
+
+  })
+
+
   getVideoUrl(video: any): string {
     return `${environment.apiUrl}/videos/play/${video.id}`;
-  
+
   }
 }
